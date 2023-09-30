cb3f8ed9f6add7325684f57613db6fd3d68ba9d5
Abhil Vidhyadharan <Abhil.Vidhyadharan@StoneX.com>
2023-09-30 19:29:51 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/mydatabase', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Create a Person schema
+const personSchema = new mongoose.Schema({
+  name: String,
+  address: String,
+  phone: String,
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+// Middleware to parse JSON data
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Serve the index.html file
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+// Handle form submission
+app.post('/form', async (req, res) => {
+  const { name, address, phone } = req.body;
+
+  try {
+    const newPerson = new Person({ name, address, phone });
+    await newPerson.save();
+    res.status(201).json({ message: 'Data saved successfully' });
+  } catch (err) {
+    res.status(500).json({ error: 'Error saving data to the database' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
